a3a45095466ce5302508b1d78e10806dee9dcc77
unknown <YSong@AMSDCWAS551YS00.usda.net>
2017-01-25 23:08:26 -0500

    added Dynamic Form application

--- src/app/dynamicForm/df.module.ts
+++ src/app/dynamicForm/df.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core'
 import {ReactiveFormsModule } from '@angular/forms'
 
+import {DynamicFormComponent} from './dynamic-form.component';
+import {DynamicFormQuestionComponent} from './dynamic-form-question.component'
 @NgModule({
     imports:[ReactiveFormsModule],
     providers:[],
-    declarations: [ ]
+    declarations: [DynamicFormComponent, DynamicFormQuestionComponent]
 
 })
 export class DfModule{
---
+++ src/app/dynamicForm/dynamic-form.component.ts
@@ -0,0 +1,6 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector: "my-dynamic-form"
+})
+export class DynamicFormComponent{}
