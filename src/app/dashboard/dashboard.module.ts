f401e42fd1d93d7950f19e24fa3b01c1672f4d1a
Yukuan Song <yukuan.song@ams.usda.gov>
2017-03-23 11:53:03 -0400

    dashboard

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,18 @@
+import{ NgModule } from '@angular/core'
+
+import {DashboardComponent} from './dashboard.component'
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        DashboardComponent,
+    ],
+    exports: [
+        DashboardComponent,
+    ]
+})
+export class DashboardModule{
+
+
+}
